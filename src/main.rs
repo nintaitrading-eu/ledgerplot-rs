2cd33a55d42ec37c6b846468a63aba54a2d50b30
Andy Nagels <thereisanewway@gmail.com>
2019-03-04 14:27:19 +0100

    Start of implementation of command execution.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod enums;
 
 use docopt::Docopt;
 use enums::plot;
+use std::process::Command;
 
 const VERSION: &'static str = "0.1.0";
 const USAGE: &'static str = "
@@ -26,9 +27,9 @@ Options:
     -h --help                   Show this screen.
     --version                   Show version.
 ";
-const CMD_INCOMEVSEXPENSES_INCOME: &'static str = "ledger -f %s --strict -j reg --real -X EUR -H ^income %s --collapse --plot-amount-format=\"%(format_date(date, \"%Y-%m-%d\")) %(abs(quantity(scrub(display_amount))))\n";
-const CMD_INCOMEVSEXPENSES_EXPENSES: &'static str = "ledger -f %s --strict -j reg --real -X EUR -H ^expenses %s --collapse";
-const CMD_INCOMEVSEXPENSES_PLOT: &'static str = "plot for [COL=STARTCOL:ENDCOL] '%s' u COL:xtic(1) w histogram title columnheader(COL) lc rgb word(COLORS, COL-STARTCOL+1), for [COL=STARTCOL:ENDCOL] '%s' u (column(0)+BOXWIDTH*(COL-STARTCOL+GAPSIZE/2+1)-1.0):COL:COL notitle w labels textcolor rgb \"#839496\"";
+const CMD_INCOMEVSEXPENSES_INCOME: &'static str = "ledger -f {file} --strict -j reg --real -X EUR -H ^income {period} --collapse --plot-amount-format=\"%(format_date(date, \"%Y-%m-%d\")) %(abs(quantity(scrub(display_amount))))\n";
+const CMD_INCOMEVSEXPENSES_EXPENSES: &'static str = "ledger -f {file} --strict -j reg --real -X EUR -H ^expenses {period} --collapse";
+const CMD_INCOMEVSEXPENSES_PLOT: &'static str = "plot for [COL=STARTCOL:ENDCOL] '{data_income}' u COL:xtic(1) w histogram title columnheader(COL) lc rgb word(COLORS, COL-STARTCOL+1), for [COL=STARTCOL:ENDCOL] '{data_expenses}' u (column(0)+BOXWIDTH*(COL-STARTCOL+GAPSIZE/2+1)-1.0):COL:COL notitle w labels textcolor rgb \"#839496\"";
 
 fn main()
 {
@@ -65,6 +66,15 @@ fn prepare_data(afile: &str, aplot_type: plot::PlotType)
     if aplot_type == plot::PlotType::IncomeVsExpenses
     {
       println!("PlotType enum = {:?}", aplot_type);
+      // TODO: period must be a parameter
+      // TODO: The below does not work.
+      let output = Command::new(format!(CMD_INCOMEVSEXPENSES_INCOME, file=afile, period="--startyear=2014 --endyear=2019"))
+          //.arg("Hello world")
+          .output()
+          .expect("Failed to execute ledger command.");
+      println!("After command");
+      assert_eq!(b"Hello world\n", output.stdout.as_slice());
+      println!("After2 command");
     }
 }
 
