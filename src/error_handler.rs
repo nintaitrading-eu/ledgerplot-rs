e4ef77e9cec0e67aa929d2ea51679b2aec725e84
rockwolf <thereisanewway@gmail.com>
2025-10-27 21:17:18 +0100

    Add error_handler module.

---
+++ src/error_handler.rs
@@ -0,0 +1,56 @@
+/*
+ * error_handler
+ *     Custom error handling module.
+ */
+pub mod error
+{
+    use thiserror::Error;
+
+    #[derive(Error, Debug)]
+    pub enum ApplicationError
+    {
+        #[error("Error (de)serializing json.")]
+        JsonError(serde_json::Error),
+
+        #[error("Error (de)serializing json.")]
+        SerdeError(serde_json::Error),
+
+        #[error("IO error.")]
+        IoError(#[from] std::io::Error),
+
+        #[error("Unexpected error.")]
+        UnexpectedError,
+
+        #[error("Unknown asset {0}, check asset_mapping.json")]
+        UnknownAssetError(String),
+
+        #[error("Value out of range: {0}")]
+        ValueOutOfRangeError(String),
+
+        #[error("Preparation of data failed.")]
+        PrepareDataError,
+
+        #[error("Plotting failed.")]
+        PlottingError,
+
+        #[error("Conversion failed.")]
+        ConversionError,
+    }
+
+    // Implement From for converting serde_json::Error to ApplicationError
+    impl From<serde_json::Error> for ApplicationError
+    {
+        fn from(err: serde_json::Error) -> ApplicationError
+        {
+            ApplicationError::SerdeError(err)
+        }
+    }
+
+    impl From<ApplicationError> for serde_json::Error
+    {
+        fn from(_err: ApplicationError) -> serde_json::Error
+        {
+           serde_json::Error::io(std::io::Error::new(std::io::ErrorKind::Other, "Unexpected serde_json error.")) 
+        }
+    }
+}
